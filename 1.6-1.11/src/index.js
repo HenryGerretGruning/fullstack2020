482d7cde5ca4ff586b31306d4b7bf73923de1363
Henry-Gerret Grüning <artimax32@gmail.com>
2021-01-19 13:05:29 +0200

    1.9 Finished

--- 1.6-1.11/src/index.js
+++ 1.6-1.11/src/index.js
@@ -7,13 +7,27 @@ const Button = (props) => (
   </button>
 )
 const Statistics = (props) => {
-  return (
-    <div>
-    <p>all: {props.bad+props.neutral+props.good}</p>
-    <p>average: {((props.bad*-1) +(props.neutral*0) +(props.good*1)) / (props.bad+props.neutral+props.good)}</p>
-    <p>positive: {props.good/(props.bad+props.neutral+props.good) *100}%</p> 
-    </div>
-  )
+  let all = props.bad+props.neutral+props.good
+  let average = ((props.bad*-1) +(props.neutral*0) +(props.good*1)) / (props.bad+props.neutral+props.good)
+  let positive = props.good/(props.bad+props.neutral+props.good) *100
+  if (all > 0) {
+    return (
+    
+      <div>
+      <p>all: {all}</p>
+      <p>average: {average}</p>
+      <p>positive: {positive}%</p> 
+      </div>
+    )
+  } else {
+    return (
+    
+      <div>
+      <p>No feedback given</p> 
+      </div>
+    )
+  }
+  
 }
 
 const App = () => {
@@ -21,8 +35,8 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const title = "give feedback"
-  
+  const title = "Give feedback"
+  const title2 = "Statistics"
 
 
   return (
@@ -34,6 +48,7 @@ const App = () => {
     <p>Good: {good}</p>
     <p>Neutral: {neutral}</p>
     <p>Bad: {bad}</p>
+    <Header title={title2} />
     <Statistics bad={bad} good={good} neutral={neutral}/>
     </div>
   )
