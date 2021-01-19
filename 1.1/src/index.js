4677da11ab1a0c0c165e69f48cbe5227acd25e5f
Henry-Gerret Grüning <artimax32@gmail.com>
2021-01-19 11:26:26 +0200

    Part 1.3 Finished

--- 1.1/src/index.js
+++ 1.1/src/index.js
@@ -4,9 +4,9 @@ import ReactDOM from 'react-dom'
 const Content = (props) => {
   return (
     <div>
-      <Part name="Fundamentals of React"/>
-      <Part name="Using props to pass data"  />
-      <Part name="State of a component" />
+      <Part name={props.name1} />
+      <Part name={props.name2}  />
+      <Part name={props.name3} />
     </div>
   )
 }
@@ -36,21 +36,30 @@ const Header = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content part={Part} />
+      <Content  
+      name1={part1.name}
+      name2={part2.name} 
+      name3={part3.name}/>
       <Total 
-      exercises1={exercises1}
-      exercises2={exercises2} 
-      exercises3={exercises3}/>
+      exercises1={part1.exercises}
+      exercises2={part2.exercises} 
+      exercises3={part3.exercises}/>
     </div>
   )
 
